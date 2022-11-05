0ff16305e44e63cdd3ef932dce4f84aa23ea8c88
petya0111 <pmarinova0111@gmail.com>
2022-11-05 21:42:55 +0200

    vrf v2 version of randomness

--- security-tasks/contracts/05_Source_of_randomness/Solution.sol
+++ security-tasks/contracts/05_Source_of_randomness/Solution.sol
@@ -1,46 +1,79 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.7;
 
-import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
+error UpkeepNotNeeded();
 
-contract GuessTheRandomNumber is VRFConsumerBase {
-    bytes32 internal keyHash;
-    uint256 internal fee;
-    uint256 public randomResult;
-    event LogAnswer(uint answer);
+contract GuessTheRandomNumber is VRFConsumerBaseV2, KeeperCompatibleInterface {
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    address vrfCoordinatorV2 = 0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D;
+    uint64 subscriptionId = 6033;
+    bytes32 gasLane =
+        0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+    uint32 callbackGasLimit = 2500000;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+    uint randomNumberRevealed;
 
-    constructor() payable public // VRF Coordinator // LINK Token
-        VRFConsumerBase(0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D,0x326C977E6efc84E512bB9C30f76E30c160eD06FB )
-    {
-        keyHash = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
-        fee = 0.25 * 10**18; // 0.25 LINK (Varies by network)
+    event RequestedRandomNumber(uint256 indexed requestId);
+    event RandomNumber(uint256 indexed number);
+
+    constructor() payable VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
     }
 
-    // Request randomness
-    function getRandomNumber() public returns (bytes32 requestId) {
-        require(
-            LINK.balanceOf(address(this)) >= fee,
-            "Not enough LINK - fill contract with faucet"
-        );
-        return requestRandomness(keyHash, fee);
-    }
-
-    // Callback function used by VRF Coordinator
-    function fulfillRandomness(bytes32 requestId, uint256 randomness)
-        internal
+    function checkUpkeep(
+        bytes memory /* checkData */
+    )
+        public
+        view
         override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
     {
-        randomResult = randomness;
+        bool hasBalance = address(this).balance > 0;
+        upkeepNeeded = hasBalance;
+        return (upkeepNeeded, "0x0");
     }
 
-    function guess(uint _guess) public {
-        uint answer = uint(getRandomNumber()) % 100;
-        emit LogAnswer(answer);
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external override {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        // require(upkeepNeeded, "Upkeep not needed");
+        if (!upkeepNeeded) {
+            revert UpkeepNotNeeded();
+        }
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            gasLane,
+            subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            callbackGasLimit,
+            NUM_WORDS
+        );
+        // Quiz... is this redundant?
+        emit RequestedRandomNumber(requestId);
+    }
+
+    function fulfillRandomWords(
+        uint256, /* requestId */
+        uint256[] memory randomWords
+    ) internal override {
+        randomNumberRevealed = randomWords[0] % 10;
+    }
+
+    function guess(uint _guess) public payable {
+        uint answer = randomNumberRevealed;
 
         if (_guess == answer) {
             (bool sent, ) = msg.sender.call{value: 1 ether}("");
             require(sent, "Failed to send Ether");
         }
+        emit RandomNumber(answer);
     }
 }
 
